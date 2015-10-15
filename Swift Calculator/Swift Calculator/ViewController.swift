7345e89d83d267e7f0d682e2bd5660bdfa5aff8d
ganchau <gancchau@gmail.com>
2015-10-15 17:58:59 -0400

    updated view controller and added calculator brain

--- Swift Calculator/Swift Calculator/ViewController.swift
+++ Swift Calculator/Swift Calculator/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController
     
     var userIsInTheMiddleOfTypingANumber = false
     
+    var brain = CalculatorBrain()
+    
     @IBAction func appendDigit(sender: UIButton)
     {
         let digit = sender.currentTitle!
@@ -32,64 +34,32 @@ class ViewController: UIViewController
                 display.text = digit
             }
             userIsInTheMiddleOfTypingANumber = true
-        }
-        
-//        print("digit = \(digit)")
+        }        
     }
     
     @IBAction func operate(sender: UIButton)
     {
-        let operation = sender.currentTitle!
-        
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
         
-        switch operation {
-        case "×": performDoubleDigitOperation { $1 * $0 }
-        case "÷": performDoubleDigitOperation { $1 / $0 }
-        case "+": performDoubleDigitOperation { $1 + $0 }
-        case "−": performDoubleDigitOperation { $1 - $0 }
-        case "sin": performSingleDigitOperation { sin($0 * M_PI / 180) }
-        case "cos": performSingleDigitOperation { cos($0 * M_PI / 180) }
-        case "√": performSingleDigitOperation { sqrt($0) }
-        default: break
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
         }
     }
-    
-    @IBAction func clear()
-    {
-        display.text = "0"
-        history.text = "0"
-        userIsInTheMiddleOfTypingANumber = false
-        operandStack = []
-        print("operandStack = \(operandStack)")
-    }
-    
-    func performDoubleDigitOperation(operation: (Double, Double) -> Double)
-    {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performSingleDigitOperation(operation: Double -> Double)
-    {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    var operandStack = Array<Double>()
     
     @IBAction func enter()
     {
         userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
-        
-        print("operandStack = \(operandStack)")
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
     }
     
     var displayValue: Double {
